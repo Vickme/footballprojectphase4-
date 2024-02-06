069dcf9a17c39149c827aeb2af0b91958331aef4
Clement Macharia <clementmacharia62@gmail.com>
2024-02-06 12:10:12 +0300

    added migrations

--- server/app.py
+++ server/app.py
@@ -1,1 +1,14 @@
 # app
+from flask import Flask,make_response,request
+from flask_migrate import Migrate
+# from flask_restful import Api
+from models import db,Team
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///football.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app,db)
+db.init_app(app)
+# api = Api(app)
--- server/models.py
+++ server/models.py
@@ -1,1 +1,16 @@
 # models
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class Team(db.Model,SerializerMixin):
+    __tablename__ = 'teams'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+    start_year = db.Column(db.DateTime)
+    coach = db.Column(db.String)
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
